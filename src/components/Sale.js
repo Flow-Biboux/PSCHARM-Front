e764f4ebb1267d630b91130968f5c1794f4d18f6
morphess47@hotmail.com <77507107+Janbyte@users.noreply.github.com>
2021-10-25 15:47:30 +0800

    feat(Sale): update css

--- src/components/Sale.js
+++ src/components/Sale.js
@@ -6,28 +6,28 @@ import { smallBreakPoint } from '../css/variables'
 function Sale() {
     return (
         <Container id="sale">
-            <SaleSection 
-                title={"Presale"}
-                price={"$0.0018"}
-                textFirstLine={"1% of total supply available"}
-                textSecondLine={"capped to 2 SOL per Wallet"}                
-                barStatus={"100"}
-                closed={true}        
+            <SaleSection
+                title={'Presale'}
+                price={'$0.0018'}
+                textFirstLine={'1% of total supply available'}
+                textSecondLine={'capped to 2 SOL per Wallet'}
+                barStatus={'100'}
+                closed={true}
             />
-            <SaleSection 
-                title={"Private Sale"}
-                price={"$0.005"}
-                textFirstLine={"5% of total supply available capped to"}
-                textSecondLine={"20 SOL per wallet. To reduce volatility"}                
-                barStatus={"soon"}
+            <SaleSection
+                title={'Private Sale'}
+                price={'$0.005'}
+                textFirstLine={'5% of total supply available capped to'}
+                textSecondLine={'20 SOL per wallet. To reduce volatility'}
+                barStatus={'soon'}
                 closed={false}
             />
-            <SaleSection 
-                title={"Public Sale"}
-                price={"$0.01"}
-                textFirstLine={"10% of total supply available with"}
-                textSecondLine={"no cap per wallet"}                
-                barStatus={"soon"}
+            <SaleSection
+                title={'Public Sale'}
+                price={'$0.01'}
+                textFirstLine={'10% of total supply available with'}
+                textSecondLine={'no cap per wallet'}
+                barStatus={'soon'}
                 closed={false}
             />
         </Container>
@@ -41,27 +41,28 @@ const Container = styled.div`
     background-color: rgb(43, 0, 0);
 
     overflow: hidden;
-    
+
     width: 100%;
-    /* height: 100vh; EDIT LATER ?  remove */
-    min-height: 40vh; /* EDIT LATER ? remove ? */
+    min-height: 80vh;
 
     margin-top: 30px;
     margin-bottom: 60px;
 
     display: flex;
-    justify-content: space-evenly;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
 
     .closed {
         opacity: 50%;
     }
 
-    @media screen and (max-width: ${smallBreakPoint}) {        
+    @media screen and (max-width: ${smallBreakPoint}) {
         display: flex;
         flex-direction: column;
         align-items: center;
         justify-content: center;
-       
+
         height: inherit;
 
         margin: 20px 0;
