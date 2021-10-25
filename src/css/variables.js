d6ece81584ee8ea957e7d8b3246db82d12b95791
morphess47@hotmail.com <77507107+Janbyte@users.noreply.github.com>
2021-10-25 15:48:35 +0800

    feat(variables): extract scrollWithOffset

--- src/css/variables.js
+++ src/css/variables.js
@@ -2,3 +2,11 @@ export const smallBreakPoint = "640px";
 export const mediumBreakPoint = "768px";
 export const largeBreakPoint = "1064px";
 
+
+export const scrollWithOffset = (el) => {
+    const yCoordinate = el.getBoundingClientRect().top + window.pageYOffset
+    let yOffset = 126
+    if (window.innerWidth <= 640) yOffset = 90
+
+    window.scrollTo({ top: yCoordinate - yOffset, behavior: 'smooth' })
+}
